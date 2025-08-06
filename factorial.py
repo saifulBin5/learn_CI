80d4c58bfbdbe5d238e988a100786342c8e852a2
Saiful I <saifulis.4965@gmail.com>
2025-08-06 15:12:41 +0530

    change

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def factorial(n):
-    if n  0:
-        raise ValueError("Input must be a non-negative integer.")
+    if n  0
+    raise ValueError("Input must be a non-negative integer.")
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
